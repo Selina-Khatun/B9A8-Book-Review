aac3cc39e4442d8b150bec2d991f138c8dcdb085
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2024-03-26 22:53:41 +0600

    added error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
 
+import video from '../../assets/Oops - 112707.mp4'
 const ErrorPage = () => {
     return (
-        <div className=' h-screen flex justify-center items-center'>
-            <h1 className=' text-5xl text-rose-500'>404</h1>
-            <h1> This page is not found</h1>
+        <div className=' container max-w-5xl mx-auto '>
+        <h1>Oops! Something went wrong...</h1>
+        <div>
+        <video autoPlay muted loop  >
+          <source src={video} type="video/mp4" />
+        </video>
         </div>
+      </div>
     );
 };
 
